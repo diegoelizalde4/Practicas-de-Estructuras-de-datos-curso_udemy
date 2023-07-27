64895a6625708823d55a80004d38fc8901bc9b0b
diegoelizalde4 <diegoelizalde475@gmail.com>
2023-07-27 10:41:53 -0600

    Se termino el ejercicio de pilas

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -16,7 +16,31 @@ using namespace std;
 
 int main(){
 
-    stack<int>
+    int n;
+    cin>>n;
+
+    stack<int> pila;
+    int value;
+    int count = 0;
+
+    for (int i = 0; i<n/2; i++){
+        cin>>value;
+        pila.push(value);
+        ++count;
+    }
+    if(n%2 !=0) cin>>value; count++;
+
+    bool palind = true;
+
+    while(cin>>value and palind and count<n){
+        if(value != pila.top()) palind= false;
+        pila.pop();
+        count++;
+    }
+
+    if(palind)cout<<"SI";
+    else cout<<"No";
+    cout<<endl;
 
 
 
